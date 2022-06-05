eebe3332d87962e81cdcaba4be56d424bd52fdf4
Fabian Suchett <fabian.suchett@edfabs.com>
2022-06-05 08:00:17 -0500

    The while cycle was wrong in the condition

--- factorial/factorial.py
+++ factorial/factorial.py
@@ -1,7 +1,7 @@
 num = int(input("Enter the number to do the factorial: "))
 i = 0
-factorial = 0
-while i == num:
+factorial = 1
+while i != num:
     factorial = factorial * (num - i)
     i = i + 1
 print(f"The factorial of the number entered is: {factorial}")
